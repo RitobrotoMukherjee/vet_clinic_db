4080a2744f2aa84fc603890a9eb7481ff9291611
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-08-08 21:01:05 +0530

    Add schema SQL

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id integer PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg decimal NOT NULL
 );
