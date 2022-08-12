0047652a9bea6d5c4c4ce2b2698c760f10d7452d
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-08-12 13:13:03 +0530

    Add join table and vets schema

--- schema.sql
+++ schema.sql
@@ -35,3 +35,28 @@ ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
 
 ALTER TABLE animals
 ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(150) NOT NULL,
+    age INT,
+    date_of_graduation date
+);
+
+/* Join tables */
+
+CREATE TABLE specializations (
+    species_id INT NOT NULL,
+    vet_id INT NOT NULL,
+    PRIMARY KEY(species_id, vet_id),
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animal_id INT NOT NULL,
+    vet_id INT NOT NULL,
+    PRIMARY KEY(animal_id, vet_id),
+    CONSTRAINT fk_animals FOREIGN KEY(animal_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
